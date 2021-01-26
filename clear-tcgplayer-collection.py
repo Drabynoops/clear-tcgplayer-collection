8cfa7bfee60aec9a88a0366ce25e6f465889e492
Drabynoops <brendan.bard@gmail.com>
2021-01-26 14:39:04 -0500

    Made function into a class
    Not sure if it was necessary or not, and it might have broken it, but hey hey, oh well

--- clear-tcgplayer-collection.py
+++ clear-tcgplayer-collection.py
@@ -8,25 +8,36 @@ TCGPLAYER_PASSWORD = "password"
 MAX_CLICK = 50 #Largest number in your collection
 SLEEP_TIME = 5 #Varies with internet speed. 5 is default
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://www.tcgplayer.com/login?returnUrl=store.tcgplayer.com%2Fcollection")
+class TcgplayerCollectionClearer:
 
-sleep(SLEEP_TIME)
-email = driver.find_element_by_name("Email")
-email.send_keys(TCGPLAYER_EMAIL)
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-password = driver.find_element_by_name("Password")
-password.send_keys(TCGPLAYER_PASSWORD)
+    def login(self):
+        self.driver.get("https://www.tcgplayer.com/login?returnUrl=store.tcgplayer.com%2Fcollection")
 
-sleep(SLEEP_TIME)
-password.send_keys(Keys.RETURN)
+        sleep(SLEEP_TIME)
+        email = self.driver.find_element_by_name("Email")
+        email.send_keys(TCGPLAYER_EMAIL)
 
-sleep(SLEEP_TIME)
-down_arrows = driver.find_elements_by_class_name("CollectionDownArrow")
+        password = self.driver.find_element_by_name("Password")
+        password.send_keys(TCGPLAYER_PASSWORD)
 
-for arrow in down_arrows:
-    for i in range(MAX_CLICK):
-        try:
-            arrow.click()
-        except:
-            pass
+        sleep(SLEEP_TIME)
+        password.send_keys(Keys.RETURN)
+
+        sleep(SLEEP_TIME)
+    
+    def clear_collection(self):
+        down_arrows = self.driver.find_elements_by_class_name("CollectionDownArrow")
+
+        for arrow in down_arrows:
+            for i in range(MAX_CLICK):
+                try:
+                    arrow.click()
+                except:
+                    pass
+
+collection_clearer = TcgplayerCollectionClearer()
+collection_clearer.login()
+collection_clearer.clear_collection()
